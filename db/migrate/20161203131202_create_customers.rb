1a6f40b12f0c404f6375951330801651376fc6c8
Jason Sim <jasonsim@gmail.com>
2016-11-23 19:55:21 -0600

    Generated Customer and User models

---
+++ db/migrate/20161203131202_create_customers.rb
@@ -0,0 +1,17 @@
+class CreateCustomers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :city
+      t.string :country
+      t.string :postal_code
+      t.string :email
+      t.string :phone
+      t.references :province, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
