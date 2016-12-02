77041042d33133d180d4e66835d1fc5330e920be
Jason Sim <jasonsim@gmail.com>
2016-12-01 19:35:21 -0600

    Removed partial. Used default formtastic syntax, Category view now working

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,4 +1,15 @@
 ActiveAdmin.register Product do
   permit_params :name, :description, :price, :stock, :size, :category_id
-  form partial: 'form'
+  form(html: { multipart: true }) do |f|
+
+  f.inputs 'Product' do
+    f.input :name
+    f.input :description
+    f.input :price
+    f.input :stock
+    f.input :category_id, selected: '1', as: :select, collection: Category.all.map { |u| [u.name.to_s, u.id] }
+    f.input :size
+  end
+    f.actions
+  end
 end
